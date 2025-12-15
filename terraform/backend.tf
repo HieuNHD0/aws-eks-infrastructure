310f82d8e0b0022728b78a80af4b53b8b5bf576d
Hieu Nguyen <hieu.nguyen@go1.com>
2025-12-15 21:14:10 +0700

    add backend

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "hieuslab-terraform"
+    key            = "aws-eks-infrastructure/terraform.tfstate"
+    region         = "ap-southeast-1"
+
+    dynamodb_table = "aws-eks-terraform-state"
+    encrypt        = true
+  }
+}
