4f0f12aaef89b991738ddfa686be60e8f7ff8464
Hieu Nguyen <hieu.nguyen@go1.com>
2025-12-15 23:25:54 +0700

    add destroy

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "hieuslab-terraform"
-    key            = "aws-eks-infrastructure/terraform.tfstate"
-    region         = "ap-southeast-1"
+    bucket = "hieuslab-terraform"
+    key    = "aws-eks-infrastructure/terraform.tfstate"
+    region = "ap-southeast-1"
 
     dynamodb_table = "aws-eks-terraform-state"
     encrypt        = true
